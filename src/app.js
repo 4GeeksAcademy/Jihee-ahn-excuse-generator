1b8f7c60515af2804a129035e66b3f948e53f368
Jahn1295 <jiheeahn1222@gmail.com>
2024-06-28 21:51:56 +0000

    done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pTag = document.getElementById("excuse");
+  let who = ["My dad", "My brother", "BTS", "My dog", "God"];
+  let action = ["cooked", "donated", "sacrificed", "digested"];
+  let what = ["my homework", "my code", "my guitar", "my resume", "my tips"];
+  let when = [
+    "while I was having the runs",
+    "while I was at work",
+    "when I was asleep",
+    "when I was praying"
+  ];
+  function generateSentence() {
+    let whoRandom = who[Math.floor(Math.random() * who.length)];
+    let actionRandom = action[Math.floor(Math.random() * action.length)];
+    let whatRandom = what[Math.floor(Math.random() * what.length)];
+    let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+    return `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}.`;
+  }
+
+  const excuseSentence = generateSentence();
+  pTag.innerHTML = excuseSentence;
+
+  console.log(excuseSentence);
 };
